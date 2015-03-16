20ced53455be802aad17b97a228bb9a4ce0c765a
Andrew James <andrewpjames87@gmail.com>
2015-03-16 14:26:40 -0600

    Setup Twitter Api model and environment

---
+++ app/models/twitter_fetcher.rb
@@ -0,0 +1,10 @@
+class TwitterFetcher
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+    end
+  end
+
+  
+end
