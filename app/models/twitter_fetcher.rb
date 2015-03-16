70f4020deea9e386045a8668b8ba111229fedf31
Andrew James <andrewpjames87@gmail.com>
2015-03-16 14:54:47 -0600

    Updated added the tokens and put them in secrets

--- app/models/twitter_fetcher.rb
+++ app/models/twitter_fetcher.rb
@@ -1,10 +1,20 @@
 class TwitterFetcher
+
+  attr_reader :client
+
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['CONSUMER_KEY']
-      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.consumer_key        = Rails.application.secrets.consumer_key
+      config.consumer_secret     = Rails.application.secrets.consumer_secret
+      config.access_token        = Rails.application.secrets.access_token
+      config.access_token_secret = Rails.application.secrets.access_token_secret
     end
   end
 
-  
+  def top
+    @client.search("to:barackobama thanks", :result_type => "recent").take(3).collect do |tweet|
+      "#{tweet.text}"
+    end
+  end
+
 end
